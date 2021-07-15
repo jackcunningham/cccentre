5c13a0215e1c3dc2fc09f5fa304992ae587dc8b4
jackcunningham <jacktcunningham@gmail.com>
2021-07-15 15:27:27 +0100

    cookie policy

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,10 +1,11 @@
-(function($) {
-    
+$(document).ready(function() {
+
+  // Set current year in copyright notice
+  const currentYear = new Date().getFullYear();
+  $('#year').text(currentYear);
+
+  // Set up accordions
   var allPanels = $('.accordion > dd').hide();
-
-
-
-
     
   $('.accordion > dt > a').click(function() {
       $this = $(this);
@@ -22,10 +23,29 @@
     $('#map').slideToggle( [], [ mapResize() ] );
   });
 
-})(jQuery);  
+});  
 
 
 function mapResize() {
-  console.log("ball bag");
+  console.log("Map resize");
   // google.maps.event.trigger(map, 'resize');
 };
+
+const cookieNotice = document.getElementById('cookie-notice');
+const acceptButton = document.querySelector('#cookie-notice button')
+
+const shouldShowCookieNotice = () => !localStorage.getItem('hasAcceptedCookies');
+const showCookieNotice = () => cookieNotice.classList = 'section section__footer';
+const acceptAndHideNotice = () => {
+  cookieNotice.classList += ' is-hidden';
+  localStorage.setItem('hasAcceptedCookies', true);
+}
+
+window.onload = () => {
+
+	if(shouldShowCookieNotice()) {
+		showCookieNotice();
+	}
+
+  acceptButton.addEventListener('click', acceptAndHideNotice, false);
+}
